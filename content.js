ca86639f755531725eb2a41becc152137d4b97cb
smeikx <smeikx@users.noreply.github.com>
2020-11-13 01:41:50 +0100

    select fonts; clean up styling; insert title clones via JS

--- content.js
+++ content.js
@@ -1,20 +1,36 @@
 'use strict';
 
-const main = document.getElementsByTagName('main')[0];
+const
+	main = document.getElementsByTagName('main')[0],
+	articles = document.getElementsByTagName('article');
 
 // Set up the scrolling titles.
 {
+
 	const titles = document.getElementsByClassName('title');
-	for (let i = titles.length - 1, title, width, parent_style; i >= 0; --i)
+	for (let i = titles.length - 1, title, width, parent, parent_style; i >= 0; --i)
 	{
 		title = titles[i];
+
+		// Insert a <span> to separate from title-clones.
+		// XXX cannot contain a terminating space, because then BoundingClientRect.width is wrong!
+		// XXX Whitespace is instead defined via CSS.
+		title.innerHTML += '<span class="separator">~</span>';
 		width = title.getBoundingClientRect().width;
-		// title’s parent should be of class ‘title-container’
-		parent_style = title.parentElement.style;
+
+		// title’s parent is of class ‘title-container’
+		parent_style = (parent = title.parentElement).style;
 		parent_style.setProperty('--title-width', '-' + width + 'px');
 		
-		// Set random scroll speed to titles; divide to normalise between different title lengths
+		// Set random scroll speed to titles; divide to normalise between different title lengths.
 		parent_style.animationDuration = width / random_int(80, 200) + 's';
+
+		// Append clones of the title to allow infinite side scrolling.
+		const clone_count = Math.ceil(articles[i].getBoundingClientRect().width / width);
+		title.outerHTML +=
+			'<span class="title-clone">' +
+			title.innerHTML.repeat(clone_count) +
+			'</span>';
 	}
 
 	// This is necessary for Chromium, because it only uses the updated CSS variable when the animation is started AFTER the variable is set.
@@ -22,7 +38,6 @@ const main = document.getElementsByTagName('main')[0];
 }
 
 {
-	const articles = document.getElementsByTagName('article');
 	for (let i = articles.length - 1; i >= 0; --i)
 	{
 		const article = articles[i];
