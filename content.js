da1477ba57133b09e04f2505ef973e02fbbc1764
smeikx <smeikx@users.noreply.github.com>
2020-12-03 18:27:03 +0100

    improve infinite horizontal scrolling
    - move code from loop to function
    - use (mosty likely) more efficient method of adding clones
    - make the page’s title scroll too
    - fix problem with ‘about’ (use <i> instead of <h1>)

--- content.js
+++ content.js
@@ -1,46 +1,80 @@
 'use strict';
 
-const
-	main = document.getElementsByTagName('main')[0],
-	articles = document.getElementsByTagName('article');
+/** @fn prepare_scrolling_title
+ *
+ *	This clones an Element until there are enough copies to fill a specified width. A separating character is inserted between the copies. The CSS property ‘--title-width’ (necessary for the scrolling animation) is set to the title element’s width.
+ *
+ * @param {Element} title  The element to be cloned.
+ * @param {Number} display_width  The width to be covered. (The titlebar of an article for example.)
+ * @param {Number} scroll_speed  This sets a certain CSS property. The number does not correspond with a specific unit.
+ */
+
+const prepare_scrolling_title = (title, display_width, scroll_speed) =>
+{
+	const separator = document.createElement('span');
+	separator.innerHTML = '~';
+	separator.className = 'separator';
+	title.appendChild(separator);
+
+	const
+		computed_style = window.getComputedStyle(title),
+		width = title.getBoundingClientRect().width + parseFloat(computed_style.marginLeft) + parseFloat(computed_style.marginRight),
+		parent = title.parentElement,
+		parent_style = parent.style,
+		clone_count = Math.ceil(display_width / width);
+
+	parent_style.setProperty('--title-width', width + 'px');
+
+	// Set scroll speed to titles; divide to normalise between different title lengths.
+	parent_style.animationDuration = width / scroll_speed + 's';
+
+	const
+		fragment = document.createDocumentFragment(),
+		title_clone = document.createElement('span');
+
+	// XXX: If I would populate the fragment with clones and just append it to title’s parent, the browser would insert whitespace after the original title.
+	fragment.appendChild(title);
+
+	title_clone.innerHTML = title.innerHTML;
+	title_clone.className = 'title-clone';
+	for (let i = 0; i <= clone_count; ++i)
+		fragment.appendChild(title_clone.cloneNode(true));
+
+	parent.appendChild(fragment);
+}
 
 // Set up the scrolling titles.
 {
+	const
+		articles = document.getElementsByTagName('article'),
+		titles = document.querySelectorAll('article .title');
 
-	const titles = document.getElementsByClassName('title');
-	for (let i = titles.length - 1, title, width, parent, parent_style; i >= 0; --i)
+	for (let i = titles.length - 1; i >= 0; --i)
 	{
-		title = titles[i];
-
-		// Insert a <span> to separate from title-clones.
-		// XXX cannot contain a terminating space, because then BoundingClientRect.width is wrong!
-		// XXX Whitespace is instead defined via CSS.
-		title.innerHTML += '<span class="separator">~</span>';
-		width = title.getBoundingClientRect().width;
-
-		// title’s parent is of class ‘title-container’
-		parent_style = (parent = title.parentElement).style;
-		parent_style.setProperty('--title-width', '-' + width + 'px');
-		
-		// Set random scroll speed to titles; divide to normalise between different title lengths.
-		parent_style.animationDuration = width / random_int(80, 200) + 's';
-
-		// Append clones of the title to allow infinite side scrolling.
-		const clone_count = Math.ceil(articles[i].getBoundingClientRect().width / width);
-		title.outerHTML +=
-			'<span class="title-clone">' +
-			title.innerHTML.repeat(clone_count) +
-			'</span>';
+		prepare_scrolling_title(
+			titles[i],
+			articles[i].getBoundingClientRect().width,
+			random_int(80, 200));
 	}
 
-	// This is necessary for Chromium, because it only uses the updated CSS variable when the animation is started AFTER the variable is set.
-	main.style.setProperty('--animation-state', 'running');
-}
+	// XXX: This is necessary for Chromium, because it only uses the updated CSS variable when the animation is started AFTER the variable is set.
+	document.body.style.setProperty('--animation-state', 'running');
 
-{
 	for (let i = articles.length - 1; i >= 0; --i)
 	{
 		const article = articles[i];
 		article.addEventListener('click', () => article.classList.toggle('open'));
 	}
 }
+
+{
+	const
+		header = document.getElementsByTagName('header')[0],
+		width = header.getBoundingClientRect().width,
+		titles = header.getElementsByClassName('title');
+
+	for (let i = titles.length - 1; i >= 0; --i)
+		prepare_scrolling_title(titles[i], width, 100);
+}
+
+// TODO: update titles on resize
