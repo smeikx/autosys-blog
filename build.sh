b4940c5eea77275128f64f784e6371a947407e93
smeikx <smeikx@users.noreply.github.com>
2021-02-14 22:24:24 +0100

    write build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+posts_dir='posts'
+
+test -d output && rm -r output
+
+# Get all post paths in alphabetical (chronological) order …
+post_list="$(find $posts_dir -maxdepth 1 -not -ipath posts | sort -r)"
+
+printf 'Post paths:\n%s\n' "$post_list"
+
+# … and reduce them to one line.
+post_list="$(printf '%s\n' $post_list | tr '\n' ' ')"
+
+echo '- - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+
+lua generate.lua $post_list
